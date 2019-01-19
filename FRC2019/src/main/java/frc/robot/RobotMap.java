5bf6626920d9b9f6fa6974198514c3ae0f350346
Gamers890 <35283424+Gamers890@users.noreply.github.com>
2019-01-19 10:03:12 -0700

    Added climb subsystem to Robot.java
    added subsystem in Robot.java and added objects in RobotMap.java.

--- FRC2019/src/main/java/frc/robot/RobotMap.java
+++ FRC2019/src/main/java/frc/robot/RobotMap.java
@@ -23,4 +23,8 @@ public class RobotMap {
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
   // public static int rangefinderModule = 1;
+  public static int frontClimbEncoderID = 0;
+  public static int backClimbEncoderID = 0;
+  public static int frontClimbMotorID = 30; 
+  public static int backClimbMotorID = 31;
 }
