56485ff060094ba5b05a76352c10722ad4a8b89d
TrashPaenda <benvigil2001@gmail.com>
2019-01-26 11:52:38 -0700

    Update ClimbControlCommand.java

--- src/main/java/frc/robot/commands/ClimbControlCommand.java
+++ src/main/java/frc/robot/commands/ClimbControlCommand.java
@@ -29,7 +29,7 @@ public class ClimbControlCommand extends Command {
   @Override
   protected void execute() {
     Robot.climbsubsystem.setClimbLegsBack(OI.getRightTriggerAxis());
-    Robot.climbsubsystem.setClimbLegsFront(OI.getLeftTriggerAxis());
+    Robot.climbsubsystem.setClimbLegsFront(-OI.getLeftTriggerAxis());
   }
 
   // Make this return true when this Command no longer needs to run execute()
