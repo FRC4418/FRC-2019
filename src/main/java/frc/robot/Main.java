55c3b207b8fdaee4ef621890a83e528b257d2ccd
WaltWe <walter.weiffenbach@stemhigh.org>
2019-01-28 19:50:29 -0800

    finished CAN Post
    finished can post. checks firmware, if we are close to competition, and duplicate ids

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -8,8 +8,12 @@
 package frc.robot;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotBase;
@@ -32,37 +36,104 @@ public final class Main {
 
     // START RIO POST
 
-    System.out.println("\n******************** Start CAN Test ********************\n");
+    // START CAN TEST
+    System.out.println("\n\n\n\n\n\n******************** Start CAN Test ********************\n\n\n\n\n\n");
+
+    // Remind when close to competition dates to check firmware
+    LocalDate date = LocalDate.now();
+    if(date.getMonth().equals(Month.MARCH)){
+      System.out.println("Make sure the firmware is up to date. It's close to competition!");
+      DriverStation.reportWarning("Make sure the firmware is up to date. It's close to competition!", false);
+    }
+
+    // Grab expected Talon IDs, the directory where CAN Data is stored on the RIO, and all the files
     int[] expectedTalonIDs = RobotMap.expectedTalonIDs;
     File CANDevicesDIR = new File("/tmp/frc_versions/");
+    File[] files = CANDevicesDIR.listFiles();
+
+    // Loop through all the CAN Files, checking for duplicate IDs and inconsistent firmware
+    String firmwareVersion = "";
+    for(int i = 0; i < files.length; i++){
+      try{
+        Scanner scanner = new Scanner(files[i]);
+        // Go through all lines in the file
+        while(scanner.hasNextLine()){
+          String line = scanner.nextLine();
+          // Nothing to compare PDP firmware to, so skip
+          if(!files[i].getName().contains("PDP")){
+            // If we have no firmware version to compare against, grab the one in this file
+            if(firmwareVersion.equals("")){
+              if(line.contains("currentVersion")){
+                firmwareVersion = line.substring(line.indexOf("=") + 1);
+              }
+            // Check if the firmware version stored and in the file are different. If they are, log it
+            }else{
+              if(line.contains("currentVersion")){
+                if(!firmwareVersion.equals(line.substring(line.indexOf("=") + 1))){
+                  firmwareVersion = line.substring(line.indexOf("=") + 1);
+                  System.out.println("There is inconsistent Talon Firmware. ");
+                  DriverStation.reportWarning("There is inconsistent Talon Firmware", false);
+                }
+              }
+            }
+          }
+          // Check if there are two devices on the id
+          if(line.contains("There are 2 devices with this Device ID")){
+            System.out.println("There are two devices of type " + files[i].getName().substring(0, files[i].getName().indexOf('-')) 
+                               + " on ID " + files[i].getName().substring(files[i].getName().indexOf('-') + 1, 
+                               files[i].getName().indexOf('-') + 3));
+            DriverStation.reportError("There are two CAN Devices on the Same ID! Check the console!", false);
+          }
+        }
+        scanner.close();
+        // Catch if there is not a file
+      } catch(FileNotFoundException e){
+        System.out.println("Something went horribly wrong. A CAN File that should exist does not. HELP!!!!!");
+        DriverStation.reportError("Something went horribly wrong. A CAN File that should exist does not.  HELP!!!!!", false);
+      }
+    }
+
+    // Grab all the filenames
     ArrayList<String> filenames = new ArrayList<String>(Arrays.asList(CANDevicesDIR.list()));
+    // We do not need to check the RIO's java version
     if(filenames.contains("FRC_Lib_Version.ini")){
       filenames.remove("FRC_Lib_Version.ini");
     }
+    // Loop through all expected Talons. Remove all filenames that we expect. 
     for(int i = 0; i < expectedTalonIDs.length; i++){
       if (filenames.contains("Talon SRX-" + expectedTalonIDs[i] + "-versions.ini")) {
         filenames.remove("Talon SRX-"+expectedTalonIDs[i]+"-versions.ini");
         System.out.println("Found expected TalonSRX on ID " + expectedTalonIDs[i]);
       }
     }
+    // PDP will always be in device range 0-10. Check if it exists. 
+    boolean pdpExists = false;
     for(int i = 0; i < 10; i++){
       if(filenames.contains("PDP-"+i+"-versions.ini")){
         filenames.remove("PDP-"+i+"-versions.ini");
         System.out.println("Found expected PDP on ID " + i);
+        pdpExists = true;
       }
     }
+    if(!pdpExists){
+      System.out.println("Did not find PDP in reserved ID range");
+      DriverStation.reportWarning("Did not find PDP in reserved ID range", false);
+    }
+    // If all the CAN files were expected, there should be nothing in the filenames arraylist
     if(filenames.size()==0){
       System.out.println("Found no unexpected devices");
+    // Report if you find unexpected devices
     }else{
-      DriverStation.reportError("CAN POST Failed. Check Console. ", false);
+      DriverStation.reportError("Found unexpected CAN Devices. Check Console. ", false);
       for(int i = 0; i < filenames.size(); i++){
         String devFileName = filenames.get(i);
         System.out.println("Found unexpected device of type " + devFileName.substring(0, devFileName.indexOf('-')) + " on ID "
         + devFileName.substring(devFileName.indexOf('-')+1, devFileName.indexOf('-')+2));
       }
     }
-    System.out.println("\n******************** End CAN Test ********************\n");
+    System.out.println("\n\n\n\n\n\n******************** End CAN Test ********************\n\n\n\n\n\n");
     
+    // END CAN TEST
 
     // END RIO POST
 
