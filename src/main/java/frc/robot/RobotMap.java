08e9204019b7f787a82f4ee957088b153e1d3f09
TrashPaenda <benvigil2001@gmail.com>
2019-02-15 19:59:01 -0700

    final
    test worked

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -75,14 +75,14 @@ public class RobotMap {
   public static final double CLIMBER_ENCODER_DISTANCE_PER_PULSE = 360d/256;
 
   // Hatch Subsystem IDs
-  public static final int HATCHE_ARM_JOINT_TALON_SRX_ID = 11, 
+  public static final int HATCHE_ARM_JOINT_TALON_SRX_ID = 40, 
                           HATCH_ARM_JOINT_ENCODER_CHANNELA_ID = 0, 
                           HATCH_ARM_JOINT_ENCODER_CHANNELB_ID = 1;
   public static final double HATCH_ARM_ENCODER_DISTANCE_PER_PULSE = 360d/256;
 
   // Drive Subsystem IDs
   public static final int DRIVE_LEFT_A_TALON_SRX_ID = 10, 
-                          DRIVE_LEFT_B_TALON_SRX_ID = 15, //11
+                          DRIVE_LEFT_B_TALON_SRX_ID = 11,
                           DRIVE_RIGHT_A_TALON_SRX_ID = 20, 
                           DRIVE_RIGHT_B_TALON_SRX_ID = 21, 
                           DRIVE_LEFT_ENCODER_CHANNELA_ID = 6, 
