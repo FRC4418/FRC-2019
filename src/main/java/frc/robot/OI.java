3d5c46ffe2cf7176e51aafc9aa74cbe1096f9026
TrashPaenda <benvigil2001@gmail.com>
2019-02-25 19:44:39 -0700

    a thing
    climb on gamepad

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,4 +1,4 @@
-/*----------------------------------------------------------------------------*/
+//////////////////////////////*----------------------------------------------------------------------------*/
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
@@ -37,10 +37,10 @@ public class OI {
   }
 
   public static double getFrontClimberAxis() {
-    return X3D_RIGHT.getRawAxis(RobotMap.FRONT_CLIMBER_AXIS_ID);
+    return GAMEPAD.getRawAxis(RobotMap.FRONT_CLIMBER_AXIS_ID);
   }
   public static double getBackClimberAxis() {
-    return X3D_LEFT.getRawAxis(RobotMap.BACK_CLIMBER_AXIS_ID);
+    return GAMEPAD.getRawAxis(RobotMap.BACK_CLIMBER_AXIS_ID);
   }
 
   public static double getHatchArmAxis() {
